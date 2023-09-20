57b5c754056c51737b55efd267744b279bf8339f
Alan Curilem <alan.curilem@umag.cl>
2023-09-20 01:23:58 -0300

    cambio nombres tablas convención gorm

--- Databases/Postgresql/veterinaria_citiaps.sql
+++ Databases/Postgresql/veterinaria_citiaps.sql
@@ -1,52 +1,54 @@
-CREATE TABLE public.dueno (
+CREATE TABLE public.duenos (
     id serial PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     edad INT NOT NULL,
     sexo VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE public.perro (
+CREATE TABLE public.perros (
     id serial PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     raza VARCHAR(255) NOT NULL,
     color VARCHAR(255) NOT NULL,
     edad INT NOT NULL,
     dueno_id INT NOT NULL,
-	FOREIGN KEY (dueno_id) REFERENCES dueno (id) ON DELETE CASCADE
+	FOREIGN KEY (dueno_id) REFERENCES duenos (id) ON DELETE CASCADE
 );
 
-CREATE TABLE public.vacuna (
+CREATE TABLE public.vacunas (
     id serial PRIMARY KEY,
     fecha date,
     nombre_vacuna VARCHAR(255),
     perro_id INT NOT NULL,
-	FOREIGN KEY (perro_id) REFERENCES perro (id) ON DELETE CASCADE
+	FOREIGN KEY (perro_id) REFERENCES perros (id) ON DELETE CASCADE
 );
 
-CREATE TABLE imagen (
+CREATE TABLE imagenes (
 	id serial PRIMARY KEY,
 	url VARCHAR (255) NOT NULL,	
 	perro_id INT NOT NULL,
-	FOREIGN KEY (perro_id) REFERENCES perro (id) ON DELETE CASCADE
+	FOREIGN KEY (perro_id) REFERENCES perros (id) ON DELETE CASCADE
 );
 
-INSERT INTO public.dueno (nombre, edad, sexo) VALUES('Pedro',	21,	'M');
-INSERT INTO public.dueno (nombre, edad, sexo) VALUES('Camila',	26,	'M');
-INSERT INTO public.dueno (nombre, edad, sexo) VALUES('Juan',	31,	'M');
-INSERT INTO public.dueno (nombre, edad, sexo) VALUES('Esteban',	53,	'M');
-INSERT INTO public.dueno (nombre, edad, sexo) VALUES('Paola',	66,	'F');
+INSERT INTO public.duenos (nombre, edad, sexo) VALUES('Pedro',	21,	'M');
+INSERT INTO public.duenos (nombre, edad, sexo) VALUES('Camila',	26,	'M');
+INSERT INTO public.duenos (nombre, edad, sexo) VALUES('Juan',	31,	'M');
+INSERT INTO public.duenos (nombre, edad, sexo) VALUES('Esteban',	53,	'M');
+INSERT INTO public.duenos (nombre, edad, sexo) VALUES('Paola',	66,	'F');
 
 
-INSERT INTO public.perro (nombre, raza, color, edad, dueno_id) VALUES('Snoopy',	'Terranova',	'Cafe',	2,	1);
-INSERT INTO public.perro (nombre, raza, color, edad, dueno_id) VALUES('Pelos',	'Labrador',	'Negro-Cafe',	4,	2);
-INSERT INTO public.perro (nombre, raza, color, edad, dueno_id) VALUES('Feliz',	'Boxer',	'Negro',	5,	3);
-INSERT INTO public.perro (nombre, raza, color, edad, dueno_id) VALUES('Max',	'Schnauzer',	'Gris',	1,	4);
-INSERT INTO public.perro (nombre, raza, color, edad, dueno_id) VALUES('Snop',	'Basset',	'Cafe-Blanco',	2,	5);
+INSERT INTO public.perros (nombre, raza, color, edad, dueno_id) VALUES('Snoopy',	'Terranova',	'Cafe',	2,	1);
+INSERT INTO public.perros (nombre, raza, color, edad, dueno_id) VALUES('Pelos',	'Labrador',	'Negro-Cafe',	4,	2);
+INSERT INTO public.perros (nombre, raza, color, edad, dueno_id) VALUES('Feliz',	'Boxer',	'Negro',	5,	3);
+INSERT INTO public.perros (nombre, raza, color, edad, dueno_id) VALUES('Max',	'Schnauzer',	'Gris',	1,	4);
+INSERT INTO public.perros (nombre, raza, color, edad, dueno_id) VALUES('Snop',	'Basset',	'Cafe-Blanco',	2,	5);
 
 
-INSERT INTO public.vacuna (fecha, nombre_vacuna, perro_id) VALUES('2023-03-22',	'Sextuple',	1);
-INSERT INTO public.vacuna (fecha, nombre_vacuna, perro_id) VALUES('2019-08-01',	'Sextuple',	2);
-INSERT INTO public.vacuna (fecha, nombre_vacuna, perro_id) VALUES('2020-12-30',	'Antirrabica',	3);
-INSERT INTO public.vacuna (fecha, nombre_vacuna, perro_id) VALUES('2022-03-22',	'Octuple2019',	4);
-INSERT INTO public.vacuna (fecha, nombre_vacuna, perro_id) VALUES('2023-12-16',	'Quintuple',	5);
+INSERT INTO public.vacunas (fecha, nombre_vacuna, perro_id) VALUES('2023-03-22',	'Sextuple',	1);
+INSERT INTO public.vacunas (fecha, nombre_vacuna, perro_id) VALUES('2019-08-01',	'Sextuple',	2);
+INSERT INTO public.vacunas (fecha, nombre_vacuna, perro_id) VALUES('2020-12-30',	'Antirrabica',	3);
+INSERT INTO public.vacunas (fecha, nombre_vacuna, perro_id) VALUES('2022-03-22',	'Octuple2019',	4);
+INSERT INTO public.vacunas (fecha, nombre_vacuna, perro_id) VALUES('2023-12-16',	'Quintuple',	5);
 
+INSERT INTO public.imagenes (url, perro_id) VALUES('/imagenes/imagen_no_disponible.png',	1);
+INSERT INTO public.imagenes (url, perro_id) VALUES('/imagenes/imagen_no_disponible.png',	2);
